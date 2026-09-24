94c791be9a715359517e1636fb7f97ca572b32ed
Pura Fome <samuelpe06062000@gmail.com>
2026-09-23 23:58:29 -0300

    teste

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://REPLACE_WITH_DEPLOYED_SERVER_URL',
+  apiBaseUrl: 'https://meurpg-a4l1.onrender.com',
   inviteBaseUrl: 'https://purafome.github.io/meuRPG/',
 };
